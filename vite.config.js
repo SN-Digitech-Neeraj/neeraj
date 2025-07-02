15e4cfd6159c2721e4f28585d831e60ad58adad0
SN-Digitech-Neeraj <neerajsndigitech1@gmail.com>
2025-07-02 15:49:05 +0530

    vite changes

--- vite.config.js
+++ vite.config.js
@@ -6,14 +6,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/',
   plugins: [
-    react({
-      // Optional: If you want to use Babel plugins
-      babel: {
-        plugins: ['@babel/plugin-transform-react-jsx'],
-      },
-    }),
-    // Uncomment to visualize the bundle size
-    // visualizer({ open: true }),
+    react(),
   ],
   optimizeDeps: {
     include: ['react', 'react-dom'],
