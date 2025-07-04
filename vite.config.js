9fdfca81760260625da6cbf3b1fc8ce5f03fd0c1
SN-Digitech-Neeraj <neerajsndigitech1@gmail.com>
2025-07-04 13:07:05 +0530

    vite is updated

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
-})
-
-
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+      },
+    },
+    chunkSizeWarningLimit: 2000, // increase this to 2000kb or as needed
+  },
+});
